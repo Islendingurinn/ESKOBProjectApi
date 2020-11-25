f5e4254089eeb5994fd92a6441e0c6b7aedc2e48
Arni Bent Thráinsson <arni0114@easv365.dk>
2020-11-25 16:14:00 +0100

    Add comment functionality

---
+++ ESKOBApi/Controllers/CommentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using ESKOBApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ESKOBApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ILogger<IdeasController> _logger;
+
+        public CommentsController(ILogger<IdeasController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Create([FromBody] Comment comment)
+        {
+            using (var _context = new ESKOBDbContext())
+            {
+                comment.Date = DateTime.Now;
+                _context.Comments.Add(comment);
+                _context.SaveChanges();
+
+                return new HttpResponseMessage(){};
+            }
+        }
+    }
+}
